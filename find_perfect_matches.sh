5a2c9e87c7a65bf1cd9cd09a8a3e24fad018b3c6
hsyyy <heshiyu49@gmail.com>
2024-09-09 15:19:52 -0400

    store chr startid endid information in temp.txt

--- find_perfect_matches.sh
+++ find_perfect_matches.sh
@@ -3,5 +3,8 @@ blastn\
 	-query $1\
 	-subject $2\
 	-task blastn-short\
-	-outfmt "6 std qlen " | awk '$3==100.000 && $4==$13' > $3
-wc -l $3
+	-outfmt "6 std qlen " | awk '$3==100.000 && $4==$13' > | awk '{print $1"\t"$9"\t"$10}' > temp.txt
+
+
+
+# extract end and start as start and end of spacer sequence, and form output file .BED
