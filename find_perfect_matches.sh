4adda76f87dd456a1072e8dc68d2720a6324129d
hsyyy <heshiyu49@gmail.com>
2024-09-09 16:51:26 -0400

    change  to  for chr name, generate temp2.bed file and extract region in bed file to spacer.fna

--- find_perfect_matches.sh
+++ find_perfect_matches.sh
@@ -3,8 +3,22 @@ blastn\
 	-query $1\
 	-subject $2\
 	-task blastn-short\
-	-outfmt "6 std qlen " | awk '$3==100.000 && $4==$13' > | awk '{print $1"\t"$9"\t"$10}' > temp.txt
+	-outfmt "6 std qlen " | awk '$3==100.000 && $4==$13' | awk '{print $2"\t"$9"\t"$10}' > temp.txt
+
+awk '
+{
+   spacer_start=prev_end+1 
+   spacer_end=$2-1
+
+   if (spacer_start<=spacer_end){
+    print prev_chr"\t"spacer_start"\t"spacer_end
+   }
+
+   prev_chr=$1
+   prev_end=$3
+
+}
+' temp.txt | awk 'NR>1' > temp2.bed
 
 
-
-# extract end and start as start and end of spacer sequence, and form output file .BED
+seqtk subseq $2 temp2.bed > $3
